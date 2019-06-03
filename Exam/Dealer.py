b09f7f7fe224b2084327b6f3b37d1d4f8b8825a6
C-Strunge <33119378+C-Strunge@users.noreply.github.com>
2019-06-03 11:19:11 +0200

    Python finish

--- Exam/Dealer.py
+++ Exam/Dealer.py
@@ -9,46 +9,48 @@ class Dealer:
         self.dealerTurn = dealerTurn
         self.roundEnd = roundEnd
 
+    # Deal card and remove from the "deck"
     def dealCards(self, deck):
         return deck.pop()
     
+    # Method for defining card as value
     def checkSum(self):
         aces = 0
-        sum = 0
+        currentVal = 0
         for i in self.hand:
             if i.number == 'J' or i.number== 'Q' or i.number == 'K':
-                sum += 10
+                currentVal += 10
 
             elif i.number == 'A':
                 aces += 1
-
+                for i in self.hand:
+                    if i.number == 'A':
+                        currentVal += self.acesValue(currentVal, aces, i)
+                        aces -= 1
             else:
-                sum += int(i.number)
+                currentVal += int(i.number)
 
-            for i in self.hand:
-                if i.number == 'A':
-                    if self.acesValue(sum, aces):
-                        sum += 1
-                    else:
-                        sum += 11
-                    aces -= 1
+            
 
-        return sum
+        return currentVal
 
-    def acesValue(self, sum, aces):
-        if sum + 11 + aces - 1 > 21:
-            return True
-        else:
-            return False
+    # Method for defining value of aces (used in above method), to act as ace can have 2 values
+    def acesValue(self, currentVal, aces, i):
+        if i.number == 'A' and currentVal + 11 + aces - 1 > 21:
+            return 1
+        elif i.number == 'A':
+            return 11
 
+    # Make the dealer take his/hers turn, and run method to check winning hand compared to player
     def dealerTakeTurn(self, player, deck):
         self.isReveal = True
         self.printCards()
         self.checkWin(player)
         self.dealerTurn = True
+        # Dealer have to "hit" if the total is less than 16, and stand if 17 or above
         while self.dealerTurn:
-            sum = self.checkSum()
-            if sum <= 16:
+            currentVal = self.checkSum()
+            if currentVal <= 16:
                 print("Dealer has less than 16, so force-hit")
                 self.hand.append(self.dealCards(deck))
                 self.printCards()
@@ -56,41 +58,45 @@ class Dealer:
             else:
                 self.dealerTurn = False
 
+    # Print a visual of the cards dealt
     def printCards(self):
+        # When comparing hands, dealer must "show" both cards
         if self.isReveal:
             for i in self.hand:
                 print(f"{i.number} of {i.suit}")
             print("Total: ", self.checkSum())
+        # When dealer has been given cards (2), and it is players turn, dealer has 1 hidden card
         if not self.isReveal:
             print("Dealer hand:")
             print(f"{self.hand[1].number} of {self.hand[1].suit}")
             print("1 hidden card")
 
+    # Dealer method for the comparing of hands, and defining the outcome based on different conditions
     def checkWin(self, player):
         playerSum = player.checkSum()
-        sum = self.checkSum()
-        if sum > 21:
+        currentVal = self.checkSum()
+        if currentVal > 21:
             print("Dealer is bust (more than 21)")
             player.balance += player.bet * 2
             player.printBalance()
             self.dealerTurn = False
             self.roundEnd = True
         
-        elif sum > playerSum and sum > 16:
-            print(f"Dealer has: {sum}, and wins. \n Player has: {playerSum}")
+        elif currentVal > playerSum and currentVal > 16:
+            print(f"Dealer has: {currentVal}, and wins. \n Player has: {playerSum}")
             player.balance = player.balance - player.bet
             player.printBalance()
             self.dealerTurn = False
             self.roundEnd = True
 
-        elif sum == playerSum and sum > 16:
-            print(f"Tied game, both has: {sum}")
+        elif currentVal == playerSum and currentVal > 16:
+            print(f"Tied game, both has: {currentVal}")
             player.printBalance()
             self.dealerTurn = False
             self.roundEnd = True
 
-        elif sum < playerSum and sum > 16:
-            print(f"Dealer lost with: {sum}. Player got: {playerSum}")
+        elif currentVal < playerSum and currentVal > 16:
+            print(f"Dealer lost with: {currentVal}. Player got: {playerSum}")
             player.balance += player.bet * 2
             player.printBalance()
             self.dealerTurn = False
